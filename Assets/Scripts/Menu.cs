b1eb8d3270e80653d5b08f00759b20bb91cd6330
DauThiTieuDiep <2115196@dlu.edu.vn>
2024-04-18 19:34:52 +0700

    sounds

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,11 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
 	public GameObject panel;
-    public void LoadScene(string sceneName)
+	public GameObject settingSound;
+	public void LoadScene(string sceneName)
     {
 		StartCoroutine(FadeIn(sceneName));
     }
@@ -20,5 +21,16 @@ public class Menu : MonoBehaviour
 		panel.SetActive(true);
 		yield return new WaitForSeconds(0.5f);//yied tra ve trong vai giay. xem canh Mantia Dot Lourdes truoc khi bat dau canh Game
 		SceneManager.LoadScene(sceneName);//tai mot scene moi trong game
-	}	
+	}
+	public void ToggleOptionsPanel()
+	{
+		if (settingSound.activeSelf) // Nếu bảng tùy chọn đang hiển thị
+		{
+			settingSound.SetActive(false); // Ẩn bảng tùy chọn
+		}
+		else
+		{
+			settingSound.SetActive(true); // Hiển thị bảng tùy chọn
+		}
+	}
 }
